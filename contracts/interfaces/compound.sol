917188d6af48e6caa2ea3c9b9a74c947d2f5348e
Di-ashX <ashutosh@hakimo.ai>
2022-02-03 05:16:26 +0530

    repay and borrow

--- contracts/interfaces/compound.sol
+++ contracts/interfaces/compound.sol
@@ -30,3 +30,41 @@ interface CErc20 {
         address collateral
     ) external returns (uint256);
 }
+
+interface Comptroller {
+    function markets(address)
+        external
+        view
+        returns (
+            bool,
+            uint256,
+            bool
+        );
+
+    function enterMarkets(address[] calldata)
+        external
+        returns (uint256[] memory);
+
+    function getAccountLiquidity(address)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    function closeFactorMantissa() external view returns (uint256);
+
+    function liquidationIncentiveMantissa() external view returns (uint256);
+
+    function liquidateCalculateSeizeTokens(
+        address cTokenBorrowed,
+        address cTokenCollateral,
+        uint256 actualRepayAmount
+    ) external view returns (uint256, uint256);
+}
+
+interface PriceFeed {
+    function getUnderlyingPrice(address cToken) external view returns (uint256);
+}
